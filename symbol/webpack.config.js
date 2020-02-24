a4d2490126e107e36149fb8e077173fab3ef4263
Mojtaba <mojtabamarashee@gmail.com>
2020-02-24 17:52:58 +0330

    test

---
+++ symbol/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {'symbol': './Symbol.js'},
+	output: {
+		path: path.resolve(__dirname),
+		filename: '[name]/dist/bundle.js',
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				options: {presets: ['@babel/env']},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			
+		],
+	},
+	resolve: {extensions: ['*', '.js', '.jsx']},
+	devServer: {
+		contentBase: path.join(__dirname, 'public'),
+		port: 3000,
+		publicPath: 'http://localhost:3000/dist/',
+		//hotOnly: true,
+		open: true,
+		historyApiFallback: true,
+	},
+	plugins: [new webpack.HotModuleReplacementPlugin()],
+};
+console.log("out = " + module.exports.output.path)
